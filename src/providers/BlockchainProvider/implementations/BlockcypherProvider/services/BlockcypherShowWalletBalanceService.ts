9699a85a6ee7897c0826c01f86b616d10e1bb137
Wladimir <wladimir.code@gmail.com>
2021-01-20 21:05:14 -0300

    feat: Blockcypher show balance, exception handler

--- src/providers/BlockchainProvider/implementations/BlockcypherProvider/services/BlockcypherShowWalletBalanceService.ts
+++ src/providers/BlockchainProvider/implementations/BlockcypherProvider/services/BlockcypherShowWalletBalanceService.ts
@@ -1,30 +1,30 @@
 import network from '../network';
+import AppError from '../../../../../errors/AppError';
 
 import IWalletBalanceDTO from '../../../dtos/IWalletBalanceDTO';
 
 class BlockcypherShowWalletBalanceService {
-  public async execute(
-    publicAddress: string,
-  ): Promise<IWalletBalanceDTO | undefined> {
-    const response = await network.get(`/addrs/${publicAddress}`);
+  public async execute(publicAddress: string): Promise<IWalletBalanceDTO> {
+    try {
+      const response = await network.get(`/addrs/${publicAddress}`);
 
-    if (!response) {
-      return undefined;
+      const {
+        address,
+        final_balance,
+        balance,
+        unconfirmed_balance,
+      } = response.data;
+
+      return {
+        publicAddress: address,
+        balance: final_balance,
+        confirmedBalance: balance,
+        unconfirmedBalance: unconfirmed_balance,
+      };
+    } catch (error) {
+      const { response } = error;
+      throw new AppError(response.data.error, response.status);
     }
-
-    const {
-      address,
-      final_balance,
-      balance,
-      unconfirmed_balance,
-    } = response.data;
-
-    return {
-      publicAddress: address,
-      balance: final_balance,
-      confirmedBalance: balance,
-      unconfirmedBalance: unconfirmed_balance,
-    };
   }
 }
 
