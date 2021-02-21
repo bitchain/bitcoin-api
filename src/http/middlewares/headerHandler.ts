3e804546b35c8b3dfe71e4b9c926294006ed2a38
Wladimir <wladimir.code@gmail.com>
2021-02-21 19:08:20 -0300

    feat: Middleware, header handler

---
+++ src/http/middlewares/headerHandler.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+
+import networkConfig from '@config/network';
+
+export async function headerHandler(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  response.append('network', networkConfig.type);
+
+  next();
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -8,14 +8,16 @@ import 'express-async-errors';
 import '@repositories/index';
 
 import { errorHandler } from './middlewares/errorHandler';
+import { headerHandler } from './middlewares/headerHandler';
 import { routes } from './routes';
 
 const application = express();
 
 application.use(cors());
 application.use(express.json());
-application.use(routes);
 
+application.use(headerHandler);
+application.use(routes);
 application.use(errorHandler);
 
 const port = process.env.APPLICATION_PORT || 3333;
