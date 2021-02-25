1bf3c6c1e18d1a6ca713b3b2904aeadc7f9155fe
Wladimir <wladimir.code@gmail.com>
2021-02-25 19:48:21 -0300

    feat: Route, providers list

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import { providersRouter } from './providers.routes';
+
 import { transactionsRouter } from './transactions.routes';
 import { walletsRouter } from './wallets.routes';
 
 const routes = Router();
+
+routes.use('/providers', providersRouter);
 
 routes.use('/transactions', transactionsRouter);
 routes.use('/wallets', walletsRouter);
--- src/http/routes/providers.routes.ts
+++ src/http/routes/providers.routes.ts
@@ -1,1 +1,9 @@
-// teste
+import { Router } from 'express';
+
+import { listProviderController } from '@useCases/ProviderList';
+
+const providersRouter = Router();
+
+providersRouter.get('/', listProviderController.handle);
+
+export { providersRouter };
