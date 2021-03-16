38a44253a47469df96d00fb511ffea6f8299bdd6
Wladimir <wladimir.code@gmail.com>
2021-03-15 22:03:39 -0300

    feat: Sentry framework, config

---
+++ src/config/sentry.ts
@@ -0,0 +1,20 @@
+import { Express } from 'express';
+
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+
+export function sentryConfig(application: Express): void {
+  const integrationsHttp = new Sentry.Integrations.Http({ tracing: true });
+  const integrationsExpress = new Tracing.Integrations.Express({
+    app: application,
+  });
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [integrationsHttp, integrationsExpress],
+    tracesSampleRate: 1.0,
+  });
+
+  application.use(Sentry.Handlers.requestHandler());
+  application.use(Sentry.Handlers.tracingHandler());
+}
