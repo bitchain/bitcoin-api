be72352dced8a874c086f1b65fc5505b83b75f01
Wladimir <wladimir.code@gmail.com>
2021-08-14 14:03:53 -0300

    fix: Babel build config

--- babel.config.js
+++ babel.config.js
@@ -17,6 +17,7 @@ module.exports = {
     }],
     "transform-typescript-metadata",
     ["@babel/plugin-proposal-decorators", { "legacy": true, }],
-    ["@babel/plugin-proposal-class-properties", { "loose": true }]
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
   ]
 }
